5b0ed3fc76dceb384f0ca0f874c2b285c323c394
Josef Utbult <josef@utbult.design>
2025-08-23 11:08:11 +0200

    Init

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+mod tests;
+
+#[allow(dead_code)]
+enum DataType {
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    U128(u128),
+    USIZE(usize),
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    I128(i128),
+    ISIZE(isize),
+    BOOL(bool),
+}
